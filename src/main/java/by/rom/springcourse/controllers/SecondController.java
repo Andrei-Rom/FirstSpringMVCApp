af3c5b5230d7906f0398abf1c25d45680814faaf
Andrei <andrisrom@gmail.com>
2022-04-25 20:12:23 +0300

    Lesson17 - Spring annotation: Controller

---
+++ src/main/java/by/rom/springcourse/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package by.rom.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+    }
+}
