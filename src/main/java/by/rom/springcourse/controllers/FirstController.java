84a28865e159344f1cb6fa964caab6c10658c4fa
Andrei <andrisrom@gmail.com>
2022-04-26 19:16:40 +0300

    Lesson20 - Model. Data transfer from the controller to the view. Basic calculator

--- src/main/java/by/rom/springcourse/controllers/FirstController.java
+++ src/main/java/by/rom/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package by.rom.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,5 +24,33 @@ public class FirstController {
     public String goobyePage(@RequestParam(value = "name", required = false) String name,
                              @RequestParam(value = "surname", required = false) String surname) {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "b", required = false) String action,
+                             Model model) {
+        double result = 0.;
+        switch (action) {
+            case ("addition"):
+                result = a + b;
+                break;
+            case ("subtraction"):
+                result = a - b;
+                break;
+            case ("multiplication"):
+                result = a * b;
+                break;
+            case ("division"):
+                if (b != 0) {
+                    result = (double) a / b;
+                } else result = 0;
+                break;
+        }
+
+        model.addAttribute("calculator", action + " of " + a + " & " + b + " is: " + result);
+
+        return "first/calculator";
     }
 }
--- src/main/java/by/rom/springcourse/controllers/SecondController.java
+++ src/main/java/by/rom/springcourse/controllers/SecondController.java
@@ -1,12 +1,18 @@
 package by.rom.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SecondController {
     @GetMapping("/exit")
-    public String exit(){
+    public String exit(@RequestParam(value = "name", required = false) String name,
+                       @RequestParam(value = "surname", required = false) String surname,
+                       Model model) {
+        model.addAttribute("message", "Goodbye " + name + " " + surname);
+
         return "second/exit";
     }
 }
